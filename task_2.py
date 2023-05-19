f2ab79684da2eeabeea065c500648ae8e73f4171
Сіплий <maksdrap228@gmail.com>
2023-05-19 17:28:16 +0300

    Добавив тестування операцій OR, AND, MOD. З множенням все ж не розібрався. Пробував Алгоритм Карацуби і Штрассена. Як в одному то іншому були весь час помилки.

--- task_2.py
+++ task_2.py
@@ -26,12 +26,6 @@ class UnsignedInt:
             hex_string = '{:08x}'.format(uint) + hex_string
         return hex_string.lstrip('0') if hex_string else '0'
 
-    def get_hex_string2(self):
-        hex_string = ''
-        for uint in self.uint_array:
-            hex_string += '{:08x}'.format(uint)
-        return hex_string.lstrip('0') if hex_string else '0'
-
 
     def get_decimal_string(self):
         decimal_string = 0
@@ -61,7 +55,6 @@ class UnsignedInt:
             uint2 = other.uint_array[i] if i < len(other.uint_array) else 0
             result.uint_array.insert(0, uint1 | uint2)
         result.num = result.uint_array[0] if result.uint_array else 0
-        return result
 
     def AND(self, other):
         result = UnsignedInt()
@@ -92,23 +85,17 @@ class UnsignedInt:
                 uint |= self.uint_array[i - 1] >> (32 - r)
             result.uint_array.insert(0, uint)
         return result
+
     def ADD(self, other):
         result = UnsignedInt()
         carry = 0
-        min_len = min(len(self.uint_array), len(other.uint_array))
-        for i in range(min_len):
-            uint = self.uint_array[i] + other.uint_array[i] + carry
+        for i in range(max(len(self.uint_array), len(other.uint_array))):
+            uint1 = self.uint_array[i] if i < len(self.uint_array) else 0
+            uint2 = other.uint_array[i] if i < len(other.uint_array) else 0
+            uint = uint1 + uint2 + carry
             carry = uint >> 32
             result.uint_array.append(uint & 0xFFFFFFFF)
-        for i in range(min_len, len(self.uint_array)):
-            uint = self.uint_array[i] + carry
-            carry = uint >> 32
-            result.uint_array.append(uint & 0xFFFFFFFF)
-        for i in range(min_len, len(other.uint_array)):
-            uint = other.uint_array[i] + carry
-            carry = uint >> 32
-            result.uint_array.append(uint & 0xFFFFFFFF)
-        if carry:
+        if carry > 0:
             result.uint_array.append(carry)
         result.num = result.uint_array[0] if result.uint_array else 0
         return result
@@ -116,17 +103,14 @@ class UnsignedInt:
     def SUB(self, other):
         result = UnsignedInt()
         borrow = 0
-        min_len = min(len(self.uint_array), len(other.uint_array))
-        for i in range(min_len):
-            uint = self.uint_array[i] - other.uint_array[i] - borrow
-            borrow = 0 if uint >= 0 else 1
+        for i in range(max(len(self.uint_array), len(other.uint_array))):
+            uint1 = self.uint_array[i] if i < len(self.uint_array) else 0
+            uint2 = other.uint_array[i] if i < len(other.uint_array) else 0
+            uint = uint1 - uint2 - borrow
+            borrow = 1 if uint < 0 else 0
             result.uint_array.append(uint & 0xFFFFFFFF)
-        for i in range(min_len, len(self.uint_array)):
-            uint = self.uint_array[i] - borrow
-            borrow = 0 if uint >= 0 else 1
-            result.uint_array.append(uint & 0xFFFFFFFF)
-        if borrow:
-            raise ValueError("Subtraction resulted in a negative value.")
+        while len(result.uint_array) > 0 and result.uint_array[-1] == 0:
+            result.uint_array.pop()
         result.num = result.uint_array[0] if result.uint_array else 0
         return result
 
@@ -159,25 +143,6 @@ class UnsignedInt:
                 divisor.uint_array.pop()
         return result
 
-    def MUL(self, other):
-        result = UnsignedInt()
-        carry = 0
-        for i in range(len(self.uint_array)):
-            for j in range(len(other.uint_array)):
-                uint_product = self.uint_array[i] * other.uint_array[j]
-                position = i + j
-                while len(result.uint_array) <= position:
-                    result.uint_array.append(0)
-                uint_sum = result.uint_array[position] + uint_product + carry
-                result.uint_array[position] = uint_sum & 0xFFFFFFFF
-                carry = uint_sum >> 32
-
-        if carry > 0:
-            result.uint_array.append(carry)
-
-        return result
-
-
 numberA = UnsignedInt()
 numberB = UnsignedInt()
 numberC = UnsignedInt()
@@ -190,63 +155,10 @@ numberB.set_from_hex_string('403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb
 numberC = numberA.XOR(numberB)
 
 print(numberC.get_hex_string())
-
 numberD.set_from_hex_string('36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80')
 numberF.set_from_hex_string('70983d692f648185febe6d6fa607630ae68649f7e6fc45b94680096c06e4fadb')
 numberG = numberD.ADD(numberF)
 
-print(numberG.get_hex_string2())
-
-a = UnsignedInt()
-a.set_from_hex_string("33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc")
-b = UnsignedInt()
-b.set_from_hex_string("22e962951cb6cd2ce279ab0e2095825c141d48ef3ca9dabf253e38760b57fe03")
-c = UnsignedInt()
-c = a.SUB(b)
-
-print(c.get_hex_string2())
-
-numberA = UnsignedInt()
-numberA.set_from_hex_string('51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4')
-numberB = UnsignedInt()
-numberB.set_from_hex_string('403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c')
-numberC = numberA.OR(numberB)
-
 print(numberC.get_hex_string())
-
-numberA = UnsignedInt()
-numberA.set_from_hex_string('51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4')
-numberB = UnsignedInt()
-numberB.set_from_hex_string('403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c')
-numberC = numberA.AND(numberB)
-
-print(numberC.get_hex_string())
-
-numberA = UnsignedInt()
-numberA.set_from_hex_string('51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4')
-numberB = UnsignedInt()
-numberB.set_from_hex_string('403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c')
-numberC = numberA.MOD(numberB)
-
-print(numberC.get_hex_string())
-
-numberA = UnsignedInt()
-numberA.set_from_hex_string('7d7deab2affa38154326e96d350deee1')
-numberB = UnsignedInt()
-numberB.set_from_hex_string('97f92a75b3faf8939e8e98b96476fd22')
-numberC = numberA.MUL(numberB)
-
-print(numberC.get_hex_string())
-
-
-
-
-
-
-
-
-
-
-
 
 
